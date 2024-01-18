61b95120d1ec9a7d8219516f93d79d970ae64d09
Manan Tank <manantankm@gmail.com>
2024-01-19 02:29:14 +0530

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,11 +25,11 @@ function ExampleLink(props: { title: string; description: string; href: string }
 	return (
 		<Link
 			href={props.href}
-			className='flex outline-none border bg-neutral-950 rounded-lg border-neutral-800 p-4 hover:border-blue-500 transition-colors'
+			className='flex border bg-neutral-950 rounded-lg border-neutral-800 p-4 hover:border-blue-500 transition-colors'
 		>
 			<div>
-				<h2 className='text-xl mb-2 text-neutral-300'> {props.title} </h2>
-				<p className='text-neutral-400'> {props.description} </p>
+				<h2 className='text-xl mb-1 text-neutral-300 font-medium'> {props.title} </h2>
+				<p className='text-neutral-500'> {props.description} </p>
 			</div>
 		</Link>
 	);
