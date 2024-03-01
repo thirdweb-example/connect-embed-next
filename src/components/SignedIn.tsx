10b39c6a6811b4ba11212ae1f8530afc089b4ca2
Joaquim Verges <joaquim.verges@gmail.com>
2024-02-29 18:14:38 -0800

    fix build

--- src/components/SignedIn.tsx
+++ src/components/SignedIn.tsx
@@ -1,19 +1,15 @@
-import { ConnectWallet } from '@thirdweb-dev/react';
+import { ConnectWallet } from "@thirdweb-dev/react";
 
 export function SignedIn() {
-	return (
-		<div className='p-20'>
-			<h1 className='flex text-center text-4xl tracking-tight justify-center'>Welcome</h1>
-			<div className='h-10' />
-			<div className='flex justify-center'>
-				<ConnectWallet
-					theme='dark'
-					dropdownPosition={{
-						align: 'center',
-						side: 'bottom',
-					}}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="p-20">
+      <h1 className="flex text-center text-4xl tracking-tight justify-center">
+        Welcome
+      </h1>
+      <div className="h-10" />
+      <div className="flex justify-center">
+        <ConnectWallet theme="dark" />
+      </div>
+    </div>
+  );
 }
